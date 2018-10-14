0395c3ccaab363b41c231d372810666d54420d35
YuriyB <paganism@mail.ru>
2018-10-14 13:21:53 +0300

    modified layout

--- models.py
+++ models.py
@@ -5,11 +5,5 @@ db = SQLAlchemy()
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    contact_name = db.Column(db.String(200))
-    contact_phone = db.Column(db.String(100))
-    contact_email = db.Column(db.String(150))
-    status = db.Column(db.Integer, index=True)
     created = db.Column(db.DateTime, index=True)
     confirmed = db.Column(db.DateTime, index=True)
-    comment = db.Column(db.Text)
-    price = db.Column(db.Numeric)
